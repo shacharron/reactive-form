a48098b3e6d0f6e547cbc3f269ff5846f334ad6a
shacharon <shachar_po@giv.ad.malam.com>
2019-01-24 08:47:29 +0200

    change to reactive

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
 import { Customer } from './customer';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +9,27 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+
   customer = new Customer();
 
   constructor() { }
 
   ngOnInit() {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
+  }
+  populateTestDate(): void {
+    this.customerForm.patchValue({
+      firstName : "jack" ,
+    })
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+
   }
 }
